3e2a726093e84f57e2be2d93b48c49546511155f
Al-vallon <you@example.com>
2022-08-16 15:16:46 +0200

    works

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 'user strict'
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    const inputs = document.querySelectorAll('input');
+    const submit = document.querySelector('.submit');
+    const displayBmi = document.querySelector('.displayBmi');
+    const result = document.querySelector('.result');
+    
+
+    // console.log(inputs);
+
+    const prevent = submit.addEventListener('click', function (e){
+    e.preventDefault()
+
+        function calcualteBmi(){
+            const height = inputs[0].value;
+            const weight = inputs[1].value;
+
+            if(!height || !weight || height <=0 || weight <= 0 ){                
+                result.textContent = '❌ Oops, Enter a valid number '
+            } else {                     
+                const bmi = Math.floor(weight / (Math.pow(height / 100, 2).toFixed (1)))
+                return bmi;     
+            }
+        }
+        
+        calcualteBmi();
+        displayBmi.textContent = calcualteBmi();
+    });
+
+});
+
+
+// mettre en place les secuirte 
+//  nb negatif, limite de poids taille
+
+// recupere le resultat est le mettre dans l endroit prevu 
+
+// en fcontion du resultat affiché l'image
