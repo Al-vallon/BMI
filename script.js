12e53ac363cdd6a69a0b80e6e7a18c551978a2b5
Al-vallon <you@example.com>
2022-08-16 21:50:33 +0200

    update reset

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-'user strict'
-window.addEventListener('DOMContentLoaded', (event) => {
+"user strict";
+window.addEventListener("DOMContentLoaded", (event) => {
     const BMIData = [
         { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
         { name: "Bonne santé", color: "green", range: [18.5, 25] },
@@ -8,36 +8,46 @@ window.addEventListener('DOMContentLoaded', (event) => {
         { name: "Obésité sévère", color: "crimson", range: [35, 40] },
         { name: "Obésité morbide", color: "purple", range: 40 },
     ];
-    const inputs = document.querySelectorAll('input');
-    const submit = document.querySelector('.submit');
-    const displayBmi = document.querySelector('.displayBmi');
-    const result = document.querySelector('.result');
-    const reset = document.querySelector('.reset');
-    
+    const inputs = document.querySelectorAll("input");
+    const submit = document.querySelector(".submit");
+    const displayBmi = document.querySelector(".displayBmi");
+    const result = document.querySelector(".result");
+    const reset = document.querySelector(".reset");
 
-    // console.log(inputs);
+    function calcualteBmi() {
+        const height = inputs[0].value;
+        const weight = inputs[1].value;
 
-    submit.addEventListener('click', function (e){
-    e.preventDefault()
-
-        function calcualteBmi(){
-            const height = inputs[0].value;
-            const weight = inputs[1].value;
-
-            if(!height || !weight || height <=0 || weight <= 0 ){                
-                result.textContent = '❌ Oops, Enter a valid number '
-            } else {                     
-                const bmi = Math.floor(weight / (Math.pow(height / 100, 2).toFixed (1)))
-                return bmi;     
-            }
+        if (!height || !weight || height <= 0 || weight <= 0) {
+            error();
+            return;
+        } else {
+            const bmi = Math.floor(weight / Math.pow(height / 100, 2).toFixed(1));
+            return bmi;
         }
+    }
 
+    function error() {
+        result.textContent = "❌ Oops, Enter a valid number ";
+    }
+
+    function rebootInputs(){
+        document.querySelectorAll('input').value = '';
+    }
+
+    submit.addEventListener("click", function (e) {
+        e.preventDefault();
         calcualteBmi();
         displayBmi.textContent = calcualteBmi();
     });
 
-    reset.addEventListener('click', function (e){
-        
+    reset.addEventListener("click", function () {
+        // event.preventDefault();        
+        result.textContent = '';        
+        const height = document.getElementById('size');
+        const weight = document.getElementById('weight');
+        height.value = '';
+        weight.value = '';
+        console.log('here');
     });
-
 });
